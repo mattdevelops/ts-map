82bbf82ff459ce1450d2b6d97da400a9702fbc69
Matthew Gill <40129227+mattdevelops@users.noreply.github.com>
2022-06-20 02:26:42 +0100

    Added better colours that reflect the in-game map

--- TsMap.Canvas/SimpleMapPalette.cs
+++ TsMap.Canvas/SimpleMapPalette.cs
@@ -1,4 +1,4 @@
-﻿using System.Drawing;
+using System.Drawing;
 
 namespace TsMap.Canvas
 {
@@ -6,14 +6,14 @@ namespace TsMap.Canvas
     {
         public SimpleMapPalette()
         {
-            Background = new SolidBrush(Color.FromArgb(72, 78, 102));
-            Road = Brushes.White;
-            PrefabRoad = Brushes.White;
+            Background = new SolidBrush(Color.FromArgb(37,37,37));
+            Road = new SolidBrush(Color.FromArgb(255,200,76));
+            PrefabRoad = new SolidBrush(Color.FromArgb(255,200,76));
             PrefabLight = new SolidBrush(Color.FromArgb(236, 203, 153));
             PrefabDark = new SolidBrush(Color.FromArgb(225, 163, 56));
             PrefabGreen = new SolidBrush(Color.FromArgb(170, 203, 150)); // TODO: Check if green has a specific z-index
 
-            CityName = Brushes.LightCoral;
+            CityName = Brushes.White;
 
             FerryLines = new SolidBrush(Color.FromArgb(80, 255, 255, 255));
 
